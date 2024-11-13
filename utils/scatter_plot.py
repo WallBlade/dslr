b4da3adf206b687deae612de4ebe094451202a85
romeo <vincent-romeo@hotmail.fr>
2024-11-13 23:40:38 +0100

    Clean and working

--- utils/scatter_plot.py
+++ utils/scatter_plot.py
@@ -8,43 +8,19 @@ import mplcursors
 RED = "\033[31m"
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
-BLUE = "\033[34m"
-MAGENTA = "\033[35m"
-CYAN = "\033[36m"
-RESET = "\033[0m"  # Reset to default color
+RESET = "\033[0m"
 
-def display_correlation_graph(feature_1_y, feature_2_y):
-	x = np.arange(1, len(feature_1_y) + 1)
-	fig, ax = plt.subplots(figsize=(12, 8))
-	plt.scatter(x, feature_1_y, color='b', s=70)
-	scat = ax.scatter(x, feature_2_y, color='r', s=30)
-	title = ax.text(0.5, 1.05, "", fontsize=12, transform=ax.transAxes, ha="center")
+def correlation(data) -> float:
+	"""
+	Returns correlations coefficient between two features using Pearson correlation.
+	The closer number is from 1 or -1, the stronger the correlation is.
 
-	def update(frame):
-		if frame == 0:
-			title.set_text("Initial Defense Against the Dark Arts values")
-			new_y = feature_2_y
-		elif frame == 1:
-			new_y = feature_2_y * 100
-			title.set_text("Defense Against the Dark Arts values * 100")
-		else:
-			new_y = feature_2_y * -100
-			title.set_text("Defense Against the Dark Arts values to positive")
-		scat.set_offsets(np.c_[x, new_y])  # Update positions of points
-		return scat, title
+	parameters: data (pandas.DataFrame): The data to calculate the correlation.
+	returns: float from -1 to 1
+	"""
 
-	anim = animation.FuncAnimation(fig, update, frames=3, interval=2000)
-
-	plt.text(0.05, 0.95, 'Blue: Astronomy score\nRed: Defense Against the Dark Arts', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top', bbox=dict(boxstyle='round,pad=0.3', edgecolor='black', facecolor='white'))
-	plt.xlabel("X-axis label")
-	plt.ylabel("Y-axis label")
-	plt.grid(True)
-	plt.show()
-
-def correlation(data):
 	if data.shape[1] != 2:
-		return "ERROR"
-		# raise ValueError("The data must have 2 columns")
+		raise ValueError("The dataFrame must have 2 columns")
 
 	data = data.dropna()
 	f1 = data.iloc[:, 0:1].values
@@ -80,35 +56,14 @@ def correlation(data):
 
 	return correlation
 
-def display_all_features_correlation_graph(correlations):
-	
-	values = np.array([item["correlation"] for item in correlations])
-	categories = np.arange(0, len(correlations))
+def get_correlation_coefficients(data):
+	"""
+	This function calculates the correlation between all the features of a dataset, prints them and returns them in array.
 
-	print(values)
-	print("--------------------------------------------------------------------")
-	print(categories)
-	print("--------------------------------------------------------------------")
-	print(correlations)
-
-	fig, ax = plt.subplots(figsize=(16, 9))
-	colors = ["red" if value <= -1 else "red" if value >= 1 else "orange" if value <= 0.8 else "blue" for value in values]
-	bars = ax.bar(categories, values, color=colors)
-	
-	mplcursors.cursor(bars, hover=True).connect("add", lambda sel: sel.annotation.set_text(f"Correlation between {correlations[sel.index]['feature_1']}\nand {correlations[sel.index]['feature_2']} is {correlations[sel.index]['correlation']}"))
-
-	ax.set_title('Sample Bar Chart')
-	ax.get_xaxis().set_visible(False)  # Hides the x-axis
-	plt.xlabel('Categories')
-	plt.ylabel('Values')
-
-	# Display the chart
-	plt.show()
-
-def get_features_correlations(data):
+	parameters: data (pandas.DataFrame): The data containing all the features to calculate the correlations.
+	returns: list of dictionaries {feature_1, feature_2, correlation}
+	"""
 	results = []
-	print(data.corr())
-	print("--------------------------------------------------------------------")
 	print("\t", end="")
 	for feature in data:
 		print(feature[:4], end=".\t")
@@ -135,22 +90,75 @@ def get_features_correlations(data):
 		print(end="\n")
 	return results
 
-def main(): # Astronomy = Defense Against the Dark Arts * -100
-	np.set_printoptions(suppress=True) # Suppress scientific notation
+def display_correlation_coefficients(ax1, correlations):
+	"""
+	Display all the correlations coefficients.
+
+	parameters: ax1 (axes object): first graph, correlations (list of dictionaries): The correlations to display.
+	returns: matplotlib.animation.FuncAnimation
+	"""
+	y1 = np.array([item["correlation"] for item in correlations])
+	x1 = np.arange(0, len(correlations))
+
+	colors = ["red" if value <= -1 else "red" if value >= 1 else "orange" if value <= -0.8 else "orange" if value >= 0.80 else "green" for value in y1]
+	bars = ax1.bar(x1, y1, color=colors)
+	mplcursors.cursor(bars, hover=True).connect("add", lambda sel: sel.annotation.set_text(f"Correlation between {correlations[sel.index]['feature_1']}\nand {correlations[sel.index]['feature_2']} is {correlations[sel.index]['correlation']}"))
+	
+	ax1.set_title('Pearson correlation coefficient (-1 and 1 means a linear relationship)')
+	ax1.get_xaxis().set_visible(False)
+	ax1.set_ylabel('Pearson correlation coefficient')
+
+def display_feature_comparision(fig, ax2, data):
+	"""
+	Display the correlation between two features using a scatter plot and animation.
+
+	parameters: fig (figure object): Graph container holder, ax2: second graph, data (pandas.DataFrame): The data to calculate the correlation.
+	returns: matplotlib.animation.FuncAnimation
+	"""
+
+	feature_1_y = data['Astronomy'].values
+	feature_2_y = data['Defense Against the Dark Arts'].values
+	x2 = np.arange(1, len(feature_1_y) + 1)
+	feature_1_scatter = ax2.scatter(x2, feature_1_y, color='b', s=30, label='Astronomy grade')
+	feature_2_scatter = ax2.scatter(x2, feature_2_y, color='r', s=10, label='Defense Against the Dark Arts grade')
+	ratio = feature_1_y[0] / feature_2_y[0]
+	legend = ax2.legend(loc='upper left')
+
+	def update(frame):
+		if frame == 0:
+			legend.get_texts()[1].set_text(f"Defense Against the Dark Arts grade * -100")
+			new_y = feature_2_y * ratio
+		elif frame == 1:
+			legend.get_texts()[1].set_text("Defense Against the Dark Arts grade")
+			new_y = feature_2_y
+		feature_2_scatter.set_offsets(np.c_[x2, new_y])
+		return feature_2_scatter, legend
+
+	anim = animation.FuncAnimation(fig, update, frames=2, interval=2000)
+
+	ax2.set_title("Astronomy and Defense Against the Dark Arts grades comparison")
+	ax2.get_xaxis().set_visible(False)
+	ax2.set_ylabel("Grade")
+	return anim
+
+def main():
+	# ---- Set up the dataset ---- #
+	np.set_printoptions(suppress=True)
 	dataset_test = "./datasets/dataset_test.csv"
 	dataset_train = "./datasets/dataset_train.csv"
-
 	data = pd.read_csv(dataset_train)
+	data = data.dropna()
+	
+	# ---- Get correlations ---- #
+	feature_to_compare = data.loc[:, 'Arithmancy':'Flying']
+	correlations = get_correlation_coefficients(feature_to_compare)
 
-	feature_1_y = data.head(200)['Astronomy'].values
-	feature_2_y = data.head(200)['Defense Against the Dark Arts'].values
-
-	feature_to_compare =	data.drop('Index', axis=1).drop('Hogwarts House', axis=1).drop('First Name', axis=1
-							).drop('Last Name', axis=1).drop('Best Hand', axis=1).drop('Birthday', axis=1)
-	correlations = get_features_correlations(feature_to_compare.head(10))
-
-	display_all_features_correlation_graph(correlations)
-	# display_correlation_graph(feature_1_y, feature_2_y)
+	# ---- Display graphs ---- #
+	fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(17, 9))
+	display_correlation_coefficients(ax1, correlations)
+	anim = display_feature_comparision(fig, ax2, data)
+	plt.tight_layout()
+	plt.show()
 
 if __name__ == "__main__":
     main()
