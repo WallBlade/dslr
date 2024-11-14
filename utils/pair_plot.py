bfcb6442f3a30106a121f217859f97d35bf1b837
Romeo VINCENT <rvincent@bess-f2r5s13.clusters.42paris.fr>
2024-11-14 20:20:00 +0100

    display working

--- utils/pair_plot.py
+++ utils/pair_plot.py
@@ -13,7 +13,7 @@ def get_colors(houses):
 		elif house == 'Gryffindor':
 			colors.append('black')
 		elif house == 'Hufflepuff':
-			colors.append('yellow')
+			colors.append('orange')
 	return colors
 
 def display_pair_graph(ax, data):
@@ -25,34 +25,38 @@ def display_pair_graph(ax, data):
 	x = data.iloc[:, 1]
 	y = data.iloc[:, 2]
 	colors = get_colors(data['Hogwarts House'])
-	ax.scatter(x, y, c=colors, alpha=0.5)
-	ax.get_xaxis().set_visible(False)
-	ax.get_yaxis().set_visible(False)
+	ax.scatter(x, y, c=colors, alpha=0.5, s=1)
+	# ax.get_xaxis().set_visible(False)
+	# ax.get_yaxis().set_visible(False)
+	ax.spines['top'].set_visible(False)
+	ax.spines['right'].set_visible(False)
 
 def main():
 	np.set_printoptions(suppress=True)
 	data = pd.read_csv("datasets/dataset_train.csv")
 	data = data.drop('Defense Against the Dark Arts', axis=1)
-	# data = data.loc[:, 'Arithmancy':'Flying']
 
 	fig, axs = plt.subplots(12, 12, figsize=(16, 10))
-	i = 0
 	y = 0
+	x = 0
 
-	# display_pair_graph(axs[i, y], data[['Hogwarts House', 'Arithmancy', 'Astronomy']])
 	for feature_y in data.loc[:, 'Arithmancy':'Flying']:
 		for feature_x in data.loc[:, 'Arithmancy':'Flying']:
-			display_pair_graph(axs[i, y], data[['Hogwarts House', feature_y, feature_x]])
-			y += 1
-		y = 0
-		i += 1
-	# 		print(f"{feature_y}/{feature_x}")
+			# if x == 0:
+			# 	axs[y, x].set_xlabel(feature_x[:4])
+			# if y == 0:
+			# 	axs[y, x].set_ylabel(feature_y[:4])
+
+			display_pair_graph(axs[y, x], data[['Hogwarts House', feature_x, feature_y]])
+			x += 1
+		x = 0
+		y += 1
+
+	axs[1, 0].set_xlabel('TEST1')
+	axs[1, 1].set_xlabel('TEST2')
 	plt.tight_layout()
-	plt.axis('off')
-	for i in range(12):
-		axs[i, 0].get_yaxis().set_visible(True)
-		axs[i, 0].set_ylabel('X-axis')
-	# axs[1, 0].set_ylabel('Y-axis')
+	plt.subplots_adjust(hspace=0.1, wspace=0.1)
+
 	plt.show()
 	print("--------------------------------------------------------------------")
 	# sns.pairplot(data, hue="Hogwarts House")
