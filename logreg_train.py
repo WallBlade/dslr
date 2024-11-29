6a7afa86f1c1f682d7b5dc7fd4269db1b15122f1
romeo <vincent-romeo@hotmail.fr>
2024-11-29 12:40:14 +0100

    working on mini-batch

--- logreg_train.py
+++ logreg_train.py
@@ -17,6 +17,26 @@ def cost_function(thetas, X, y):
 	j = np.sum(y * np.log(hypothesis(thetas, X)) + (1 - y) * np.log(1 - hypothesis(thetas, X)))
 	return -1 / y.shape[0] * j
 
+def create_mini_batches(X, y, batch_size):
+    n_samples = X.shape[0]
+    
+    # Shuffle indices
+    indices = np.arange(n_samples)
+    np.random.shuffle(indices)
+    
+    # Shuffle the data
+    X_shuffled = X[indices]
+    y_shuffled = y[indices]
+    
+    # Split into mini-batches
+    mini_batches = []
+    for i in range(0, n_samples, batch_size):
+        X_batch = X_shuffled[i:i + batch_size]
+        y_batch = y_shuffled[i:i + batch_size]
+        mini_batches.append((X_batch, y_batch))
+    
+    return mini_batches
+
 def gradient(thetas, X, y):
 	m = X.shape[0]
 	sum = (1 / m) * X.T @ (hypothesis(thetas, X) - y)
@@ -24,21 +44,34 @@ def gradient(thetas, X, y):
 
 def gradient_descent(X, y):
 	thetas = np.zeros(11)
-	old_cost = cost_function(thetas, X, y)
-	i = 0
+	old_cost = float('inf')
 	learning_rate = 0.5
 
-	for i in range(1000):
-		grad = gradient(thetas, X, y)
-		thetas = thetas - learning_rate * grad
-		new_cost = cost_function(thetas, X, y)
-		# print(f"New gradient: {thetas} new_cost: {new_cost} old_cost: {old_cost} at iteration {i}")
+	for i in range(2000):
+		# new_cost = 0
+		mini_batches = create_mini_batches(X, y, 32)
+		for X_mini, y_mini in mini_batches:
+			grad = gradient(thetas, X_mini, y_mini)
+			thetas = thetas - learning_rate * grad
+			# new_cost += cost_function(thetas, X_mini, y_mini)
+		# if new_cost > old_cost or abs(old_cost - new_cost) < sys.float_info.epsilon:
+		# 	print(f"TROUVÉ at iteration {i} old_cost: {old_cost} new_cost: {new_cost}")
+		# 	return thetas
+		# old_cost = new_cost
 
-		if new_cost > old_cost or abs(old_cost - new_cost) < sys.float_info.epsilon:
-			break
+	# for i in range(100000):
+	# 	grad = gradient(thetas, X, y)
+	# 	thetas = thetas - learning_rate * grad
+	# 	# new_cost = cost_function(thetas, X, y)
+	# 	# print(f"New gradient: {thetas} new_cost: {new_cost} old_cost: {old_cost} at iteration {i}")
+
+	# 	# if new_cost > old_cost or abs(old_cost - new_cost) < sys.float_info.epsilon:
+	# 	# 	print(f"TROUVÉ at iteration {i} old_cost: {old_cost} new_cost: {new_cost}")
+	# 	# 	break
 	
-		old_cost = new_cost
-		i += 1
+	# 	# old_cost = new_cost
+	# 	# i += 1
+
 	return thetas
 
 def predict(thetas, X, df):
@@ -65,6 +98,7 @@ def main():
 	np.set_printoptions(suppress=True) # Suppress scientific notation
 	df = prep_d.prepare_data('datasets/dataset_train.csv')
 	df.insert(0, 'Bias', 1)
+
 
 	X = df.select_dtypes(include=['number']).values
 	r = []
