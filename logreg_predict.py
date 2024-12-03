af7c786d4664c557ddb7c6d346a4c6f38ec86518
Romeo VINCENT <rvincent@bess-f2r2s15.clusters.42paris.fr>
2024-12-03 17:18:57 +0100

    read thetas in file for prediction

--- logreg_predict.py
+++ logreg_predict.py
@@ -1,8 +1,9 @@
 import utils.prepare_data as prep_d
-import pandas as pd
 import numpy as np
-import time
-import sys
+
+GREEN = "\033[32m"
+RED = "\033[31m"
+RESET = "\033[0m"
 
 def hypothesis(thetas, X):
 	z = X@thetas
@@ -20,29 +21,26 @@ def predict(thetas, X, df):
 			hypothesis(thetas[3], X[i]) * 100 # Slytherin
 		])
 		index = np.argmax(prob)
-		print(f"\033[32mIndex: {i} prediction: {labels[index]} prob: {prob[index]}\033[0m")
+		print(f"{i},{labels[index]}")
 
 def main():
-	# try:
-	np.set_printoptions(suppress=True) # Suppress scientific notation
-	df = prep_d.prepare_data('datasets/dataset_test.csv')
-	df.insert(0, 'Bias', 1)
+	try:
+		np.set_printoptions(suppress=True) # Suppress scientific notation
+		df = prep_d.prepare_data('datasets/dataset_test.csv')
+		df.insert(0, 'Bias', 1)
 
-	X = df.select_dtypes(include=['number']).values
-	with open('logistic_thetas.txt', 'r') as file:
-		for line in file:
-			# r.append(list(map(float, line.strip().split())))
-			print(line)
-	# r = [[-4.73492917, -1.30059946, -2.83179215,  2.80333461,  0.42985167,
-    #     0.51813633,  3.25250019,  1.25574746, -1.17938403, -1.85469993,
-    #     5.8323784 ], [-2.24272297,  2.07262412,  2.45016615,  1.02905415, -1.28709271,
-    #    -1.60271137,  0.40124806, -0.17860478, -0.01654976, -0.67055768,
-    #    -1.13140041], [-2.36776646, -1.16608572,  1.11252704, -0.18263422,  0.74618376,
-    #     1.40804   , -0.53262887, -0.88297075,  0.0925333 ,  1.68819776,
-    #    -0.94286206], [-4.59977224, -1.97150977, -0.34322703, -1.61553238, -0.17390371,
-    #    -0.37221513, -0.09493884,  1.66789086,  0.58818215, -1.67648574,
-    #    -0.41893518]]
-	# predict(r, X, df)
+		X = df.select_dtypes(include=['number']).values
+		r = []
+
+		with open('logistic_thetas.txt', 'r') as file:
+			for line in file:
+				thetas = line.strip().split()
+				thetas = list(map(float, thetas))
+				r.append(list(map(float, line.strip().split())))
+
+		predict(r, X, df)
+	except Exception as e:
+		print(f"{RED}Error: {e}{RESET}")
 
 if __name__ == "__main__":
     main()
